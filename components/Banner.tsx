f39bd0abee58763e0aacebadb6ebfeec243f4125
TahoorAhmed <tahoor.ahmed@shottechnologies.co>
2023-07-08 04:18:25 +0500

    commited

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ import ProfileImg from "../public/img/profileImg.jpg";
 
 const Banner = () => {
   return (
-    <div className="wrapper flex flex-col md:flex-row items-center gap-10 px-3">
+    <div className="wrapper flex flex-col md:flex-row items-center gap-10 px-1">
       <div className="w-44 h-44 rounded-full bg-black">
         <Image
           className="w-full h-full rounded-full border-[1px] border-indigo-700 p-2 object-cover"
@@ -15,10 +15,10 @@ const Banner = () => {
         <h1 className="md:text-5xl text-4xl font-bold text-center">
           Tahoor Ahmed
         </h1>
-        <h3 className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
+        <h3 className="text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
           Full Stack Developer
         </h3>
-        <p className="md:text-base tracking-wide text-center md:text-left text-xs">
+        <p className="text-base tracking-wide text-center md:text-left ">
           I am a skilled MERN developer with a strong passion for creating
           efficient and impactful web applications.
         </p>
